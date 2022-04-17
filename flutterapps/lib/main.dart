87529ddd0e9fed6f396e6feb26d58579357869a2
SolisMarco <2518302017@mail.utec.edu.sv>
2022-04-17 13:52:38 -0600

    confirmar

---
+++ flutterapps/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(webFlutter());
+}
+
+class webFlutter extends StatefulWidget {
+  @override
+  State<webFlutter> createState() => _webFlutterState();
+}
+
+class _webFlutterState extends State<webFlutter> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "consumo webservice",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("webservice flutter"),
+        ),
+        body: Center(
+          child: Container(
+            child: Text("cuerpo de la aplicacion"),
+          ),
+        ),
+      ),
+    );
+  }
+}
